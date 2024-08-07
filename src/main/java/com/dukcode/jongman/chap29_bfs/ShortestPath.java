13fdb61ab586181b8bba16cfd0d167d089c9cbcd
김덕윤 <duk9741@gmail.com>
2024-08-07 11:58:55 +0900

    jongman: ìBFS 최단 경로 출력

--- src/main/java/com/dukcode/jongman/chap29_bfs/ShortestPath.java
+++ src/main/java/com/dukcode/jongman/chap29_bfs/ShortestPath.java
@@ -5,7 +5,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class ShortestPath {
@@ -19,6 +24,9 @@ public class ShortestPath {
   private static int m;
   private static List<Integer>[] adj;
 
+  private static int[] distance;
+  private static int[] parent;
+
   public static void main(String[] args) throws IOException {
     br = new BufferedReader(new InputStreamReader(System.in));
     bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,18 +35,68 @@ public class ShortestPath {
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
 
-    f
+    adj = new List[n];
+    for (int i = 0; i < n; ++i) {
+      adj[i] = new ArrayList<>();
+    }
+
+    for (int i = 0; i < m; ++i) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+
+      adj[from].add(to);
+    }
+
+    List<Integer> path = shortestPath(0, 4);
+    bw.write(Arrays.toString(path.toArray()));
 
     br.close();
     bw.close();
   }
 
-  private static void bfs() {
+  private static List<Integer> shortestPath(int from, int to) {
+    distance = new int[n];
+    Arrays.fill(distance, -1);
+    parent = new int[n];
+    Arrays.fill(parent, -1);
+
+    bfs(from);
+    return shortestPath(to);
+  }
+
+  private static void bfs(int start) {
+
+    Queue<Integer> q = new ArrayDeque<>();
+    q.offer(start);
+    distance[start] = 0;
+    parent[start] = start;
+
+    while (!q.isEmpty()) {
+      int here = q.poll();
+      for (int there : adj[here]) {
+        if (distance[there] != -1) {
+          continue;
+        }
+
+        q.offer(there);
+        distance[there] = distance[here] + 1;
+        parent[there] = here;
+      }
+    }
 
   }
 
   private static List<Integer> shortestPath(int v) {
+    List<Integer> path = new ArrayList<>();
+    path.add(v);
+    while (parent[v] != v) {
+      path.add(parent[v]);
+      v = parent[v];
+    }
 
+    Collections.reverse(path);
+    return path;
   }
 
 
