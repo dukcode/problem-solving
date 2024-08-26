1d61be8cf60cae550707bac94e1c0e394ef38001
김덕윤 <duk9741@gmail.com>
2024-08-26 15:42:26 +0900

    codetree: TransformationOfNumberSystem

---
+++ src/main/java/com/dukcode/codetree/novice_mid/TransformationOfNumberSystem.java
@@ -0,0 +1,17 @@
+package com.dukcode.codetree.novice_mid;
+
+import java.util.Scanner;
+
+public class TransformationOfNumberSystem {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    String n = sc.next();
+
+    System.out.println(Integer.toString(Integer.parseInt(n, a), b));
+  }
+
+}
